f34cca8c2cac50653f2ff48451c45e305d0aea20
cebrf <cebrf@yandex.ru>
2020-01-12 12:31:47 +1000

    add models Users and Events

---
+++ ThEvent/ThEvent/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThEvent.Models
+{
+    public class Users
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+    }
+}
