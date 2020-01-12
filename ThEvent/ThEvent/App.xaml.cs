73d4957710174f04995bf3eaa4b12d4ef6b5981f
cebrf <cebrf@yandex.ru>
2020-01-12 14:23:27 +1000

    fix data

--- ThEvent/ThEvent/App.xaml.cs
+++ ThEvent/ThEvent/App.xaml.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ThEvent.Data;
+using ThEvent.Models;
+using System.IO;
 
 namespace ThEvent
 {
     public partial class App : Application
     {
+        static ThEventDatabase database;
+        public static ThEventDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ThEventDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ThEvent.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
