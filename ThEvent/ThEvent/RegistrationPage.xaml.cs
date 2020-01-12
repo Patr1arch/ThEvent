3c893ffe4ccb9e4b002c50249d7ebb8499690824
Nikita <nikitabaranov200021@gmail.com>
2020-01-12 18:04:48 +1000

    Make Send e-mails

--- ThEvent/ThEvent/RegistrationPage.xaml.cs
+++ ThEvent/ThEvent/RegistrationPage.xaml.cs
@@ -9,6 +9,8 @@ using SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Mail;
 
 namespace ThEvent
 {
@@ -53,17 +55,40 @@ namespace ThEvent
                 String.Equals(password.Text, checkPassword.Text))
 
             {
-                App.Database.SaveUserAsync(
-                    new User
+                User newUser = new User
+                {
+                    FirstName = Name.Text,
+                    SecondName = SecondName.Text,
+                    Email = Email.Text,
+                    Password = password.Text
+                };
+                CheckEmailPage checkEmail = new CheckEmailPage();
+
+                checkEmail.Appearing += (sender_, e_) =>
+                {
+                    MailAddress from = new MailAddress("thevent2020@mail.ru", "ThEvent-no-reply");
+                    MailAddress to = new MailAddress(newUser.Email);
+                    MailMessage m = new MailMessage(from, to);
+                    m.Subject = "Код авторизации";
+                    m.Body = "<h2>Код авторизации: </h2><h1>" + checkEmail.verifyCode.ToString() + "</h1><h3>Никому ему не говорите!</h3>";
+                    m.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient("smtp.mail.ru", 2525);
+                    smtp.Credentials = new NetworkCredential("thevent2020@mail.ru", "S_fgnz4P%5");
+                    smtp.EnableSsl = true;
+                    smtp.Send(m);
+                };
+
+                checkEmail.Disappearing += (sender_, e_) =>
+                {
+                    if (checkEmail.isVerified)
                     {
-                        FirstName = Name.Text,
-                        SecondName = SecondName.Text,
-                        Email = Email.Text,
-                        Password = password.Text
-                    });
-                App.IsAnonym = false;
-                Navigation.PopAsync();
-                Navigation.PushAsync(new EventPage());
+                        App.Database.SaveUserAsync(newUser);
+                        App.IsAnonym = false;
+                        Navigation.PopAsync();
+                        Navigation.PushAsync(new EventPage());
+                    }
+                };
+                Navigation.PushAsync(checkEmail);
             }
             else
             {
