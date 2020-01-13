228652327be5d5b1c3a305137b0f937c386b9d04
kmac3063 <lantcov.iiu@students.dvfu.ru>
2020-01-13 12:05:31 +1000

    Refactor footer code, deleted copypast. Reduced size

--- ThEvent/ThEvent/Footer.cs
+++ ThEvent/ThEvent/Footer.cs
@@ -12,58 +12,46 @@ namespace ThEvent
     class Footer
     {
         public static Color Color;
+
+        private static ImageButton createImButtonForFooter(string source)
+        {
+            var imButton = new ImageButton
+            {
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Source = source,
+                WidthRequest = 40,
+                HeightRequest = 40,
+                BackgroundColor = Color,
+                Margin = new Thickness(0, 5)
+            };
+
+            return imButton;
+        }
         public static StackLayout getFooter()
         {
             Color = Color.FromHex("#90be7f");
 
-            var eventButton = new ImageButton {
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Source = "events.ico",
-                WidthRequest = 50,
-                HeightRequest = 50,
-                BackgroundColor = Color,
-                Margin = new Thickness(0, 10)
-            };
+            var eventButton = createImButtonForFooter("events.ico");
             eventButton.Clicked += async (a, b) =>
             {
                 await App.Current.MainPage.Navigation.PushAsync(new EventPage());
             };
 
-            var rulesButton = new ImageButton {
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Source = "rules.ico",
-                WidthRequest = 50,
-                HeightRequest = 50,
-                BackgroundColor = Color,
-                Margin = new Thickness(0, 20)
-            };
+            var rulesButton = createImButtonForFooter("rules.ico");
             rulesButton.Clicked += async (a, b) =>
             {
                 await App.Current.MainPage.Navigation.PushAsync(new RulesPage());
             };
 
-            var contactsButton = new ImageButton {
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Source = "contacts.ico",
-                WidthRequest = 50,
-                HeightRequest = 50,
-                BackgroundColor = Color,
-                Margin = new Thickness(0, 20)
-            };
+            var contactsButton = createImButtonForFooter("contacts.ico");
             contactsButton.Clicked += async (a, b) =>
             {
                 await App.Current.MainPage.Navigation.PushAsync(new ContactsPage());
             };
 
-            var profileButton = new ImageButton {
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                Source = "profile.ico",
-                WidthRequest = 50,
-                HeightRequest = 50,
-                BackgroundColor = Color,
-                Margin = new Thickness(0, 20)
-            };
-            profileButton.Clicked += async (a, b) => {
+            var profileButton = createImButtonForFooter("profile.ico");
+            profileButton.Clicked += async (a, b) =>
+            {
                 await App.Current.MainPage.Navigation.PushAsync(new ProfilePage());
             };
 
