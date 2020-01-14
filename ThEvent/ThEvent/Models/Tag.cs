8cffa229db9b0c84083ee485bc51f89283e9c3f3
cebrf <cebrf@yandex.ru>
2020-01-14 20:55:44 +1000

    add Tag model

---
+++ ThEvent/ThEvent/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions;
+
+namespace ThEvent.Models
+{
+    class Tag
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
